254abb9bad5cf1ab89b1f62772d4aae13b01e496
naween0 <naween803@gmail.com>
2016-12-14 09:23:26 -0500

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.Timer;
+
+class myPanel() extends JPanel {
+  public myPanel() {
+    super();
+    
+  }
+}
+
+public class Game extends JFrame {
+  
+  public Game() {
+    super("The Game");
+    
+  }
+  
+  
+  public static void main(String[] args) { 
+    
+  }
+  
+  
+}
---
+++ Player.java
@@ -0,0 +1,15 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class Player {
+  
+  public Player() { 
+    int x, y;
+    int health;
+    int speed;
+    int ammo;
+    boolean canThrow;
+    
+  }
+
+}
