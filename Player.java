6a01b7b4ba131e9b6c0b859202dbc39ff3b7fe65
naween0 <naween803@gmail.com>
2016-12-14 09:36:48 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -2,14 +2,19 @@
  * Auto Generated Java Class.
  */
 public class Player {
-  
-  public Player() { 
     int x, y;
     int health;
     int speed;
     int ammo;
     boolean canThrow;
     
+  public Player(int x, int y) { 
+    this.x = x;
+    this.y = y;
+    health = 100;
+    speed = 10;
+    ammo = 1;
+    canThrow = false;
   }
 
 }
