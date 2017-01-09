9684c47e8f8ccf0da85eee685d5c763a4fdb61fa
naween0 <naween803@gmail.com>
2017-01-08 21:40:33 -0500

    Add files via upload

---
+++ New folder/Barrel.java
@@ -0,0 +1,10 @@
+import java.awt.Rectangle;
+
+public class Barrel extends Rectangle{
+	public Barrel(int x, int y){
+		this.x = x;
+		this.y = y;
+		height = 40;
+		width = 40;
+	}
+}
---
+++ Tile.java
@@ -0,0 +1,8 @@
+
+public class Tile {
+  
+  public Tile() { 
+    
+  }
+  
+}
